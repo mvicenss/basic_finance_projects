781430cc1f86b4643cef5cadf2ca02f4af874822
mvicenss <182702132+mvicenss@users.noreply.github.com>
2025-06-26 22:37:01 +0200

    Added method implementations

--- src/main/java/com/mvicenss/finance_calculator/CalculatorMain.java
+++ src/main/java/com/mvicenss/finance_calculator/CalculatorMain.java
@@ -9,6 +9,8 @@ class CalculatorMain{
         Scanner sc = new Scanner(System.in);
         int option = sc.nextInt();
 
+        Manager manager = new Manager();
+
         switch (option){
             case 1:
                 System.out.println("What do you want to do?");
@@ -16,6 +18,31 @@ class CalculatorMain{
                 System.out.println("b. Subtract a product from ticket");
                 System.out.println("c. Get total tax value");
                 System.out.println("d. Get total value of ticket");
+
+                char subOption = sc.nextLine().charAt(0);
+
+                switch (subOption){
+                    case 'a':
+                        System.out.println("Name of the product");
+                        String name = sc.nextLine();
+                        System.out.println("Tax type");
+                        double taxType = sc.nextDouble();
+                        System.out.println("Gross price");
+                        double grossAmount = sc.nextDouble();
+
+                        Products product = new Products(name, taxType, grossAmount);
+                        manager.addProduct(product);
+                        System.out.println("The product has been added");
+                        break;
+                    case 'b':
+                        System.out.println("Name of the product");
+                        String nameToRemove = sc.nextLine();
+
+                        manager.removeByName(nameToRemove);
+                        System.out.println("The product has been removed");
+                        break;
+
+                }
             case 2:
         }
     }

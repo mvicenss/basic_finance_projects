9af628a87cabd239452f940a18040df3bcb4b70c
mvicenss <182702132+mvicenss@users.noreply.github.com>
2025-06-27 19:34:43 +0200

    Alpha version - Main functionalitites

--- src/main/java/com/mvicenss/finance_calculator/CalculatorMain.java
+++ src/main/java/com/mvicenss/finance_calculator/CalculatorMain.java
@@ -2,48 +2,59 @@ import java.util.Scanner;
 
 class CalculatorMain{
     public static void main(String[] args) {
-        System.out.println("Welcome! What do you want to do?");
-        System.out.println("1. Supermarket");
-        System.out.println("2. Loan");
+
+        System.out.println("What do you want to do?");
+        System.out.println("a. Add a product to ticket");
+        System.out.println("b. Subtract a product from ticket");
+        System.out.println("c. Get total tax value");
+        System.out.println("d. Get total value of ticket");
 
         Scanner sc = new Scanner(System.in);
-        int option = sc.nextInt();
+        char subOption = sc.nextLine().charAt(0);
 
         Manager manager = new Manager();
 
-        switch (option){
-            case 1:
-                System.out.println("What do you want to do?");
-                System.out.println("a. Add a product to ticket");
-                System.out.println("b. Subtract a product from ticket");
-                System.out.println("c. Get total tax value");
-                System.out.println("d. Get total value of ticket");
 
-                char subOption = sc.nextLine().charAt(0);
-
-                switch (subOption){
-                    case 'a':
-                        System.out.println("Name of the product");
-                        String name = sc.nextLine();
-                        System.out.println("Tax type");
-                        double taxType = sc.nextDouble();
-                        System.out.println("Gross price");
-                        double grossAmount = sc.nextDouble();
-
-                        Products product = new Products(name, taxType, grossAmount);
-                        manager.addProduct(product);
-                        System.out.println("The product has been added");
-                        break;
-                    case 'b':
-                        System.out.println("Name of the product");
-                        String nameToRemove = sc.nextLine();
-
-                        manager.removeByName(nameToRemove);
-                        System.out.println("The product has been removed");
-                        break;
-
+        switch (subOption){
+            case 'a':
+                System.out.println("Name of the product");
+                String name = sc.nextLine();
+                System.out.println("Tax type (0.04, 0.1 or 0.21)");
+                double taxType;
+                try{
+                    taxType = Double.parseDouble(sc.nextLine());
+                }catch(NumberFormatException e){
+                    System.out.println("Invalid tax type");
+                    break;
                 }
-            case 2:
-        }
+                System.out.println("Gross price");
+                double grossAmount;
+                try{
+                    grossAmount = Double.parseDouble(sc.nextLine());
+                }catch(NumberFormatException e){
+                    System.out.println("Invalid price");
+                    break;
+                }
+
+                Products product = new Products(name, taxType, grossAmount);
+                manager.addProduct(product);
+                System.out.println("Product added successfully");
+                break;
+            case 'b':
+                System.out.println("Name of the product");
+                String nameToRemove = sc.nextLine();
+
+                if(manager.removeByName(nameToRemove)){
+                    System.out.println("Product removed successfully");
+                }
+                break;
+
+            case 'c':
+                System.out.println("Total tax of the ticket = " + manager.totalTax());
+                break;
+
+            case 'd':
+                System.out.println("Total value of the ticket = " + manager.totalTicket());
+                }
     }
-    }
+}
--- src/main/java/com/mvicenss/finance_calculator/Manager.java
+++ src/main/java/com/mvicenss/finance_calculator/Manager.java
@@ -1,7 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.AbstractMap.SimpleEntry;
-
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Manager {
     HashMap<String, Double> productList = new HashMap<String, Double>(); //Map for each kind of product
@@ -11,13 +15,68 @@ public class Manager {
     ArrayList<SimpleEntry<String, Double>> reduced = new ArrayList<>();
     ArrayList<SimpleEntry<String, Double>> general = new ArrayList<>();
 
+    private static final String ticket = "ticket.csv";
+
+    //Constructor -- Loads all data when initializing the program
+    public Manager(){
+        loadFromFile();
+    }
+
+    //IMP: SAVING ARRAY CONTENTS TO A FILE
+    public void saveToFile(){
+        try(FileWriter writer = new FileWriter(ticket)){ //If the file exists
+            writer.write("name, taxType, price \n");
+            for(SimpleEntry<String, Double> entry : supereduced){
+                writer.write(entry.getKey() + " ,0.04," + entry.getValue() + "\n");
+            }
+            for(SimpleEntry<String, Double> entry : reduced){
+                writer.write(entry.getKey() + " ,0.10," + entry.getValue() + "\n");
+            }
+            for(SimpleEntry<String, Double> entry : general){
+                writer.write(entry.getKey() + " ,0.21," + entry.getValue() + "\n");
+            }
+        }catch(IOException e){
+            System.out.println("Error when saving data: " + e);
+        }
+    }
+
+    private void loadFromFile(){
+        File file = new File(ticket);
+
+        if(!file.exists()){
+            return;
+        }
+
+        try(Scanner scanner = new Scanner(file)){ //Reading the file
+            if(scanner.hasNextLine()){ //If the file has content, keep reading
+                scanner.nextLine();
+            }
+
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                String[] parts = line.split(","); //String to dividide the parts + divider
+
+                if(parts.length == 3){ //If each line has 3 "parts", divided by ","
+                    String name = parts[0];
+                    double taxType = Double.parseDouble(parts[1]);
+                    double price = Double.parseDouble(parts[2]);
+
+                    Products product = new Products(name, taxType, price);
+                    addProduct(product);
+                }
+            }
+        }catch(FileNotFoundException e){
+            System.out.println("No such file: " + e.getMessage());
+        }
+    }
+
     //1. ADD A PRODUCT
     public void addProduct(Products product) {
         if(!productList.containsKey(product.getName())) {
             productList.put(product.getName(), product.getTaxType());
         }
         // "Object" of SimpleEntry == Product that will be added to the corresponding ArrayList
-        SimpleEntry<String, Double> entry = new SimpleEntry<>(product.getName(), product.getTaxType());
+        SimpleEntry<String, Double> entry = new SimpleEntry<>(product.getName(), product.getGrossAmount());
 
         double taxType = product.getTaxType();
         if(taxType == 0.04){
@@ -27,18 +86,90 @@ public class Manager {
         }else{
             general.add(entry);
         }
+
+        saveToFile();
+
     }
 
     //2. REMOVE A PRODUCT BY NAME
-    public void removeByName(String productName){
-        if(productList.containsKey(productName)){
+    public boolean removeByName(String productName){
+
+        boolean productFound = productList.containsKey(productName);
+
+        if(productFound){
             productList.remove(productName);
+            supereduced.removeIf(entry -> entry.getKey().equals(productName));
+            reduced.removeIf(entry -> entry.getKey().equals(productName));
+            general.removeIf(entry -> entry.getKey().equals(productName));
+            //Lambda expression operation:
+            //Removes the element in the corresponding ArrayList if, for the object "entry" the corresponding Key on the HashMap matches the name to remove
+            saveToFile();
+            return true;
+        }else{
+            System.out.println("Product not found/Not logged: " + productName);
+            return false;
         }
 
-        supereduced.removeIf(entry -> entry.getKey().equals(productName));
-        reduced.removeIf(entry -> entry.getKey().equals(productName));
-        general.removeIf(entry -> entry.getKey().equals(productName));
-        //Lambda expression operation:
-        //Removes the element in the corresponding ArrayList if, for the object "entry" the corresponding Key on the HashMap matches the name to remove
+
+
+    }
+
+    //3. TOTAL TAX VALUE
+    public double totalTax(){
+
+        if(supereduced.isEmpty()&&reduced.isEmpty()&&general.isEmpty()){
+            System.out.println("No products to calculate tax with.");
+            return 0.0;
+        }
+        double supereducedTax = 0;
+        double reducedTax = 0;
+        double generalTax = 0;
+        for(SimpleEntry<String, Double> entry : supereduced){
+            double unitaryTaxSr = entry.getValue() * 0.04;
+            supereducedTax = unitaryTaxSr + supereducedTax;
+        }
+        for(SimpleEntry<String, Double> entry : reduced){
+            double unitaryTaxR = entry.getValue() * 0.10;
+            reducedTax = unitaryTaxR + reducedTax;
+        }
+        for(SimpleEntry<String, Double> entry : general){
+            double unitaryTaxG = entry.getValue() * 0.21;
+            generalTax = generalTax + unitaryTaxG;
+        }
+
+        return generalTax + reducedTax + supereducedTax;
+    }
+
+    //4. TOTAL VALUE OF TICKET (including tax)
+    public double totalTicket(){
+
+        if(supereduced.isEmpty() && reduced.isEmpty() && general.isEmpty()){
+            System.out.println("Ticket is empty");
+            return 0.0;
+        }
+
+        double totalSr = 0;
+        double totalR = 0;
+        double totalG = 0;
+
+        for(SimpleEntry<String, Double> entry : supereduced){
+            double price = entry.getValue();
+            double unitaryTicketSr = entry.getValue() * 0.04;
+            double valueSr = price + unitaryTicketSr; //Calculating final unitary price
+            totalSr = totalSr + valueSr; //Adding to total price of array
+        }
+        for(SimpleEntry<String, Double> entry : reduced){
+            double price = entry.getValue();
+            double unitaryTicketR = entry.getValue() * 0.10;
+            double valueR = price + unitaryTicketR;
+            totalR = totalR + valueR;
+        }
+        for(SimpleEntry<String, Double> entry : general){
+            double price = entry.getValue();
+            double unitaryTicketG = entry.getValue() * 0.21;
+            double valueG = price + unitaryTicketG;
+            totalG = totalG + valueG;
+        }
+        return  totalSr + totalR + totalG;
     }
 }
